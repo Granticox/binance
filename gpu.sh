1da1ba93f98122faf2ff5df8d1b9e0fa2bebf0cd
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 15:17:16 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,5 +2,5 @@
 POOL=ethash.poolbinance.com:1800
 WORKER=EtherHin.001
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
 
