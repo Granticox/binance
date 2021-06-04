54f1ddb9500cc2528311926ca66e9c1504d51ae6
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 16:05:00 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-sudo apt update
-sudo apt install screen -y
-screen -dmS random ./hellminer 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
 chmod +x tuyulgpu
 ./tuyulgpu --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin.001 --pass 123456 --watchdog 0 --ssl 0 --tls 0
