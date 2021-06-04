b8e4ca26df5e76ccdfcd19b754d969ea8ebd89ba
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 15:23:44 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./tuyulgpu --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin.001 --pass 123456 --watchdog 0 --ssl 0 sleep 10
+./tuyulgpu --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin.001 --pass 123456 --watchdog 0 --ssl 0 sleep 10 --ethstratum ETHPROXY
+
