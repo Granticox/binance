5b915cb87392bcdf1072cae20c4992986283375c
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-04 16:14:22 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x tuyulgpu
-./tuyulgpu G -P stratum+tcp://Minerhin.001:123456@ethash.poolbinance.com:1800sleep 10done
+./tuyulgpu G -server stratum+tcp://Minerhin.001:123456@ethash.poolbinance.com:1800sleep 10done
